e6a5e7216c1bbce704a1f12e0ad2cd600283fb44
Ben <wolfnun011@gmail.com>
2020-09-26 11:55:24 +0900

    feature: Added custom command 'getByDataCy' to find tags marked for testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getByDataCy', query => cy.get(`[data-cy="${query}"]`));
