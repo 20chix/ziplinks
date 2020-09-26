190fa888e7ba318a99ea4a50c78d11daea5e84c3
Ben <wolfnun011@gmail.com>
2020-09-26 12:06:12 +0900

    fix: Test to lightly check routing & landing page form elements

---
+++ cypress/intergration/landing.spec.js
@@ -0,0 +1,24 @@
+describe('Landing page test', () => {
+  it('URL for / redirects to the landing page', () => {
+    cy.visit('/').then(() => {
+      cy.url().should('include', '/landing');
+    });
+  })
+
+  it('Checks landing page contents', () => {
+    cy.visit('/').then(() => {
+      // header
+      cy.getByDataCy('landing-display-header').contains('The only link you\'ll ever need, for free');
+      // ziplinks button
+      cy.getByDataCy('ziplinks-button').contains('ziplinks.me/ziplinks');
+      // learn more button
+      cy.getByDataCy('ziplinks-button-learn-more').contains('Learn More');
+      // sign up button
+      cy.getByDataCy('ziplinks-button-sign-up').contains('Sign Up');
+      // Donations form has 4 elements (3 inputs and 1 img)
+      cy.getByDataCy('donation-form').then(form => {
+        expect(form.children()).to.have.length(4);
+      });
+    });
+  });
+})
