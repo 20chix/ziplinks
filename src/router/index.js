265a945f630da2f0924f3c1f726fb97266387f50
unknown <hadi20107@hotmail.co.uk>
2020-09-28 20:47:10 +0100

    Removed unecessary file - About

--- src/router/index.js
+++ src/router/index.js
@@ -32,14 +32,6 @@ const router = new Router({
       meta: {
         requiresAuth: true
       }
-    },
-    {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
   ]
 })
