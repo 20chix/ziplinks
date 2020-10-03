bb7953906bd8501a4f70ae31ed47d6c41ceefa6f
unknown <hadi20107@hotmail.co.uk>
2020-10-03 22:11:15 +0100

    Add component for choosing fonts

--- src/components/Fonts/chooseFonts.vue
+++ src/components/Fonts/chooseFonts.vue
@@ -1,13 +1,82 @@
 <template>
-  
+  <v-col cols="12" sm="6">
+    <v-card class="mx-auto" outlined>
+      <v-card-text>
+        <div class="display-1 text--primary">Font</div>
+      </v-card-text>
+      <v-form ref="form" lazy-validation>
+        <v-container>
+          <p class="subtitle-2 text-left">
+            Select a font from the list below. This will apply to all your
+            buttons.
+            <br />
+            <br />
+            Your current font is {{ radios || "null" }}
+          </p>
+          <v-radio-group v-model="radios">
+            <v-radio label="Default" value="Default"></v-radio>
+            <v-radio
+              style="font-family: 'Bangers'"
+              label="Bangers"
+              value="Bangers"
+            ></v-radio>
+            <v-radio
+              style="font-family: 'Lobster'"
+              label="Lobster"
+              value="Lobster"
+            ></v-radio>
+            <v-radio
+              style="font-family: 'Dancing Script'"
+              label="Dancing Script"
+              value="Dancing Script"
+            ></v-radio>
+            <v-radio
+              style="font-family: 'Architects Daughter'"
+              label="Architects Daughter"
+              value="Architects Daughter"
+            ></v-radio>
+            <v-radio
+              style="font-family: 'Pacifico'"
+              label="Pacifico"
+              value="Pacifico"
+            ></v-radio>
+          </v-radio-group>
+          <v-btn @click="editFontSelection()" block color="primary">
+            <v-icon left>fas fa-edit</v-icon>Save
+          </v-btn>
+        </v-container>
+      </v-form>
+    </v-card>
+    <br />
+  </v-col>
 </template>
 
 <script>
-export default {
+import { store } from "../../store";
+import { mapState } from "vuex";
 
-}
+export default {
+  name: "chooseFonts",
+  data() {
+    return {
+      radios: "Default",
+    };
+  },
+  methods: {
+    editFontSelection() {
+      store.dispatch("addUpdateFont", this.radios );
+    },
+  },
+  beforeMount(){
+      console.log(this.searchedUser.font)
+      this.radios = this.searchedUser.font
+
+  },
+  computed: {
+    ...mapState(["searchedUser"]),
+  },
+};
 </script>
 
 <style>
-
 </style>
